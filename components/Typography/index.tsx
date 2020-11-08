3023b8ad145951bb8ba8d8d28eac1d8283a4b6bb
vibhor <vibhor@vibhor.local>
2020-11-08 20:27:51 +0530

    text component

--- components/Typography/index.tsx
+++ components/Typography/index.tsx
@@ -145,6 +145,7 @@ const Text: React.SFC<TextProps> = ({ children, ...props }: TextProps) => {
       wordBreak={props.wordBreak}
       stringColor={props.stringColor}
       margin={props.margin}
+      {...props}
     >
       {children}
     </TextStyled>
