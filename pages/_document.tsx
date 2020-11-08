f32ef3a607740a76398b6907c46d46ada1904df9
vibhor <vibhor@vibhor.local>
2020-11-08 20:47:06 +0530

    issue resolved

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,4 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document';
+import Document, { DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -31,39 +26,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-  render() {
-    return (
-      <html lang="en">
-        <Head>
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-          />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="description" content="pwa spaceproject application" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#000" />
-          <link rel="apple-touch-icon" href="/example.png" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="pwa spaceproject application" />
-          <meta
-            property="og:description"
-            content="pwa spaceproject application"
-          />
-          <meta
-            property="og:site_name"
-            content="pwa spaceproject application"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
   }
 }
