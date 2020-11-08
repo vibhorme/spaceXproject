6cba65f9298e52d98804b55601fb377b9886451e
vibhor <vibhor@vibhor.local>
2020-11-08 20:34:41 +0530

    meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,28 +36,27 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <meta name="spacexproject" content="PWA App" />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
-            name="description"
-            content="app to show details about spacexproject"
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
           />
+          <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="512x512"
-            href="/icons/icon-512x512.png"
-          />
-
+          <meta name="description" content="pwa spaceproject application" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <link
-            rel="mask-icon"
-            href="/icons/icon-512x512.png"
-            color="#5bbad5"
+          <link rel="apple-touch-icon" href="/example.png" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="pwa spaceproject application" />
+          <meta
+            property="og:description"
+            content="pwa spaceproject application"
           />
-          <link rel="shortcut icon" href="/icons/favicon.ico" />
+          <meta
+            property="og:site_name"
+            content="pwa spaceproject application"
+          />
         </Head>
         <body>
           <Main />
