1eaecc3255a8c13ffb2ed16891e42978c2736d37
vibhor <vibhor@vibhor.local>
2020-11-08 20:51:53 +0530

    tags created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import styled from 'styled-components';
 import Flex from '../components/Flex';
 import Text from '../components/Typography';
@@ -72,6 +73,27 @@ export default function Home() {
   }, [selectedDetail]);
   return (
     <PageStyled>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="description" content="pwa spaceproject application" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000" />
+        <link rel="apple-touch-icon" href="/example.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="pwa spaceproject application" />
+        <meta
+          property="og:description"
+          content="pwa spaceproject application"
+        />
+        <meta property="og:site_name" content="pwa spaceproject application" />
+      </Head>
       <Text
         fontSize="h4"
         fontWeight="bold"
